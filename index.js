b77cdebfd1287afaa59eee22447b5306ff4d3b57
Richard <rlew0925@gmail.com>
2025-02-05 02:10:11 -0700

    Update index.js

--- index.js
+++ index.js
@@ -12,3 +12,29 @@ function fuzzyMatch(drivers, letter) {
         return driver.toLowerCase().startWith(letter.toLowerCase());
     });
 }
+
+/**
+const drivers = [
+        {
+          name: 'Bobby',
+          hometown: 'Pittsburgh' },
+        {
+          name: 'Sammy',
+          hometown: 'New York' } ,
+        {
+          name: 'Sally',
+          hometown: 'Cleveland' },
+        {
+          name: 'Annette',
+          hometown: 'Los Angeles' },
+        {
+          name: 'Bobby',
+          hometown: 'Tampa Bay' }
+      ];
+*/
+ 
+function matchName(drivers, name) {
+    return drivers.filter(driver => {
+        return driver.name === name;
+    });
+}
